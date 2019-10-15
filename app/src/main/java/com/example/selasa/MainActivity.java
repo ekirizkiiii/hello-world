7b6e783d56c74e78f7bcd5b00e819e70ce590bab
Eki Rizki Pratama Putra <ekirizki44@gmail.com>
2019-10-15 11:38:31 +0700

    Commit kedua.

--- app/src/main/java/com/example/selasa/MainActivity.java
+++ app/src/main/java/com/example/selasa/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.selasa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int skorA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void tambah3PointA(View view) {
+        skorA = skorA + 3;
+        TextView textViewA = findViewById(R.id.skorA);
+        textViewA.setText(String.valueOf(skorA));
     }
 }
