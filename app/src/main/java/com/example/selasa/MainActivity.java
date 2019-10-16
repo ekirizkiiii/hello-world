a4c21e69641886fb33c70678e73088304236e8da
Eki Rizki Pratama Putra <ekirizki44@gmail.com>
2019-10-16 11:37:27 +0700

    commit ketiga

--- app/src/main/java/com/example/selasa/MainActivity.java
+++ app/src/main/java/com/example/selasa/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private int skorA = 0;
+    private int skorB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +21,44 @@ public class MainActivity extends AppCompatActivity {
         skorA = skorA + 3;
         TextView textViewA = findViewById(R.id.skorA);
         textViewA.setText(String.valueOf(skorA));
+    }
+
+    public void tambah3PointB(View view) {
+        skorB = skorB + 3;
+        TextView textViewB = findViewById(R.id.skorB);
+        textViewB.setText(String.valueOf(skorB));
+    }
+
+    public void tambah2PointA(View view) {
+        skorA = skorA + 2;
+        TextView textViewA = findViewById(R.id.skorA);
+        textViewA.setText(String.valueOf(skorA));
+    }
+
+    public void tambah2PointB(View view) {
+        skorB = skorB + 2;
+        TextView textViewB = findViewById(R.id.skorB);
+        textViewB.setText(String.valueOf(skorB));
+    }
+
+    public void tambah1PointA(View view) {
+        skorA = skorA + 1;
+        TextView textViewA = findViewById(R.id.skorA);
+        textViewA.setText(String.valueOf(skorA));
+    }
+
+    public void tambah1PointB(View view) {
+        skorB = skorB + 1;
+        TextView textViewB = findViewById(R.id.skorB);
+        textViewB.setText(String.valueOf(skorB));
+    }
+
+    public void resetSkor(View view) {
+        skorA = skorA * 0;
+        skorB = skorB * 0;
+        TextView textViewA = findViewById(R.id.skorA);
+        textViewA.setText(String.valueOf(skorA));
+        TextView textViewB = findViewById(R.id.skorB);
+        textViewB.setText(String.valueOf(skorB));
     }
 }
